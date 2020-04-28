ca4266a64096320c367e656f7b8783cc75cb9470
Sagar Adhikari <sagradhkr48@gmail.com>
2020-04-28 22:48:56 +0545

    added addMvc to the dependency injection service and added mvc middleware also the home controller added

--- Startup.cs
+++ Startup.cs
@@ -15,7 +15,7 @@ namespace EmployeeManagement
     public class Startup
     {
         private IConfiguration _config;
-        
+
         public Startup(IConfiguration config)
         {
             _config = config;
@@ -25,6 +25,8 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options=>options.EnableEndpointRouting=false);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,45 +39,12 @@ namespace EmployeeManagement
 
             app.UseRouting();
 
-            //FileServerOptions fileServerOptions = new FileServerOptions();
-            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
-
-
-
-
-            //app.UseFileServer();
+                    //app.UseFileServer();
             app.UseStaticFiles();
-
-            //app.Use(async (context, next) =>
-            //{
-            //    await context.Response.WriteAsync("Hello World!" );
-
-            //});
-
-            app.Run(async (context) =>
-            {
-                //throw new Exception("Some error Processing the Request");
-                await context.Response.WriteAsync("Hosting Environment" + env.EnvironmentName);
-            });
+            app.UseMvcWithDefaultRoute();
 
 
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context=>
-            //    {
-            //        await context.Response.WriteAsync("Hello from first middleware!");
-            //    });
-            //});
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello from 2nd middleware!");
-            //    });
-            //});
         }
     }
+
 }
